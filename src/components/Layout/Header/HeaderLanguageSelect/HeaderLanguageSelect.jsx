0451ed9b38182c06c069cc866e5418727688a399
pashinskij99 <pashinskij99@icloud.com>
2023-07-10 23:55:20 +0300

    fix

--- src/components/Layout/Header/HeaderLanguageSelect/HeaderLanguageSelect.jsx
+++ src/components/Layout/Header/HeaderLanguageSelect/HeaderLanguageSelect.jsx
@@ -33,7 +33,7 @@ export default function HeaderLanguageSelect({ className }) {
     <StyledHeaderLanguageSelectWrapper
       onMouseEnter={handleClick}
       onMouseLeave={handleClose}
-      onTouchStart={handleClick}
+      onClick={handleClick}
       className={className}
     >
       <button
