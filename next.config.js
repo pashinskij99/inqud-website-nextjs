d6b7be86c8362bf59f1de7297e68f77a3211aa00
pashinskij99 <pashinskij99@icloud.com>
2023-07-20 20:45:00 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,31 +1,24 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  redirects: async () =>
-    isProd
-      ? [
-          {
-            source: '/:path*',
-            has: [
-              { type: 'host', value: 'inqud-website-nextjs-inky.vercel.app' },
-            ],
-            destination:
-              'https://www.inqud-website-nextjs-inky.vercel.app/:path*',
-            permanent: true,
-          },
-        ]
-      : [
-          {
-            source: '/:path*',
-            has: [{ type: 'host', value: 'localhost:3000' }],
-            destination: 'https://www.localhost:3000/:path*',
-            permanent: true,
-          },
-        ],
+  // redirects: async () =>
+  //   isProd
+  //     ? [
+  //         {
+  //           source: '/:path*',
+  //           has: [
+  //             { type: 'host', value: 'inqud-website-nextjs-inky.vercel.app' },
+  //           ],
+  //           destination:
+  //             'https://www.inqud-website-nextjs-inky.vercel.app/:path*',
+  //           permanent: true,
+  //         },
+  //       ]
+  //     : [],
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
