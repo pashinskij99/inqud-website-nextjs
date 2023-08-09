aa3dbbd84c760d84fac43873e5b0240c5aa5dd4b
pashinskij99 <pashinskij99@icloud.com>
2023-08-09 11:33:02 +0300

    fix

--- src/sections/CryptoWidgetSections/CryptoWidgetFlexibility/CryptoWidgetFlexibility.styled.jsx
+++ src/sections/CryptoWidgetSections/CryptoWidgetFlexibility/CryptoWidgetFlexibility.styled.jsx
@@ -1,3 +1,18 @@
 import styled from '@emotion/styled'
 
-export const StyledCryptoWidgetFlexibilityWrapper = styled.section``
+export const StyledCryptoWidgetFlexibilityWrapper = styled.section`
+  padding-top: 200px;
+  .container {
+    .grid {
+      grid-template-columns: repeat(3, 440px);
+      align-items: flex-start;
+      .grid-item {
+        padding: 40px 32px;
+        .grid-item-title {
+        }
+        .grid-item-description {
+        }
+      }
+    }
+  }
+`
