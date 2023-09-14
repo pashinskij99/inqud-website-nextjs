4543cfd9f0425e4ee1f668d9ab7050721946a477
yaroslav <pashinskij99@gmail.com>
2023-09-14 15:00:40 +0300

    fixes

--- src/middleware.js
+++ src/middleware.js
@@ -1,16 +1,17 @@
-import createMiddleware from 'next-intl/middleware';
+import createMiddleware from 'next-intl/middleware'
 
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'uk', 'es', 'ru'],
+  locales: ['en'],
+  // locales: ['en', 'uk', 'es', 'ru'],
 
   // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
   defaultLocale: 'en',
   localePrefix: 'never',
-});
+})
 
 export const config = {
   // Skip all paths that should not be internationalized. This example skips the
   // folders "api", "_next" and all files with an extension (e.g. favicon.ico)
   matcher: ['/((?!api|_next|.*\\..*).*)'],
-};
+}
