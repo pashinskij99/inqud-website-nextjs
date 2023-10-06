43cb16096897e503b48532bdd1713ad2cdde2df5
yaroslav <pashinskij99@gmail.com>
2023-10-06 18:39:13 +0300

    fx

--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -5,15 +5,15 @@ import { StyledHomeWrapper } from './HomePage.styled'
 // import { NotFoundContext } from '@/contexts/NotFoundContext/NotFoundContext'
 import PageProvider from '@/contexts/PageContext/PageContext'
 import FeesBusiness from '@/sections/HomeSections/FeesBusiness'
-import CryptoWidgetSection from '@/sections/HomeSections/CryptoWidgetSection'
-import SmoothAPISection from '@/sections/HomeSections/SmoothAPISection'
+// import CryptoWidgetSection from '@/sections/HomeSections/CryptoWidgetSection'
+// import SmoothAPISection from '@/sections/HomeSections/SmoothAPISection'
 import PickSection from '@/sections/HomeSections/PickSection'
 import CryptoWidget2Section from '@/sections/HomeSections/CryptoWidget2Section'
 import ReasonsToTeamUp from '@/sections/HomeSections/ReasonsToTeamUp'
-// import OurLandscapeSection from '@/sections/HomeSections/OurLandscapeSection'
-// import BlogsSection from '@/sections/HomeSections/BlogsSection'
-// import QuestionsSection from '@/sections/HomeSections/QuestionsSection'
-import HeroSection from '@/sections/HomeSections/HeroSection'
+import OurLandscapeSection from '@/sections/HomeSections/OurLandscapeSection'
+import BlogsSection from '@/sections/HomeSections/BlogsSection'
+import QuestionsSection from '@/sections/HomeSections/QuestionsSection'
+// import HeroSection from '@/sections/HomeSections/HeroSection'
 import YourNeedsSection from '@/sections/HomeSections/YourNeedsSection'
 
 export default function HomePage({ data, params }) {
@@ -30,18 +30,18 @@ export default function HomePage({ data, params }) {
   return (
     <PageProvider isFaq dataPage={data} params={params} nameCMSPage='homePage'>
       <StyledHomeWrapper>
-        <HeroSection />
+        {/* <HeroSection /> */}
         <YourNeedsSection />
-        <CryptoWidgetSection />
-        <SmoothAPISection />
+        {/* <CryptoWidgetSection /> */}
+        {/* <SmoothAPISection /> */}
         <PickSection className='pickSection' variant='dontLose' />
         <CryptoWidget2Section />
         <ReasonsToTeamUp />
         <FeesBusiness modelId='2540165' autoId={10} />
-        {/* <OurLandscapeSection /> */}
+        <OurLandscapeSection />
         <PickSection className='pickSection2' />
-        {/* <BlogsSection /> */}
-        {/* <QuestionsSection /> */}
+        <BlogsSection />
+        <QuestionsSection />
       </StyledHomeWrapper>
     </PageProvider>
   )
