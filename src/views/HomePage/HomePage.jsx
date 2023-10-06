1b380b55effd8f8cfed25fbdc5a9b0daabfa3608
yaroslav <pashinskij99@gmail.com>
2023-10-06 20:46:17 +0300

    check without home page

--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -4,17 +4,17 @@
 import { StyledHomeWrapper } from './HomePage.styled'
 // import { NotFoundContext } from '@/contexts/NotFoundContext/NotFoundContext'
 import PageProvider from '@/contexts/PageContext/PageContext'
-import FeesBusiness from '@/sections/HomeSections/FeesBusiness'
-import CryptoWidgetSection from '@/sections/HomeSections/CryptoWidgetSection'
-import SmoothAPISection from '@/sections/HomeSections/SmoothAPISection'
-import PickSection from '@/sections/HomeSections/PickSection'
-import CryptoWidget2Section from '@/sections/HomeSections/CryptoWidget2Section'
-import ReasonsToTeamUp from '@/sections/HomeSections/ReasonsToTeamUp'
-import OurLandscapeSection from '@/sections/HomeSections/OurLandscapeSection'
-import BlogsSection from '@/sections/HomeSections/BlogsSection'
-import QuestionsSection from '@/sections/HomeSections/QuestionsSection'
-import HeroSection from '@/sections/HomeSections/HeroSection'
-import YourNeedsSection from '@/sections/HomeSections/YourNeedsSection'
+// import FeesBusiness from '@/sections/HomeSections/FeesBusiness'
+// import CryptoWidgetSection from '@/sections/HomeSections/CryptoWidgetSection'
+// import SmoothAPISection from '@/sections/HomeSections/SmoothAPISection'
+// import PickSection from '@/sections/HomeSections/PickSection'
+// import CryptoWidget2Section from '@/sections/HomeSections/CryptoWidget2Section'
+// import ReasonsToTeamUp from '@/sections/HomeSections/ReasonsToTeamUp'
+// import OurLandscapeSection from '@/sections/HomeSections/OurLandscapeSection'
+// import BlogsSection from '@/sections/HomeSections/BlogsSection'
+// import QuestionsSection from '@/sections/HomeSections/QuestionsSection'
+// import HeroSection from '@/sections/HomeSections/HeroSection'
+// import YourNeedsSection from '@/sections/HomeSections/YourNeedsSection'
 
 export default function HomePage({ data, params }) {
   // const { setIsNotFound } = useContext(NotFoundContext)
@@ -30,7 +30,7 @@ export default function HomePage({ data, params }) {
   return (
     <PageProvider isFaq dataPage={data} params={params} nameCMSPage='homePage'>
       <StyledHomeWrapper>
-        <HeroSection />
+        {/* <HeroSection />
         <YourNeedsSection />
         <CryptoWidgetSection />
         <SmoothAPISection />
@@ -41,7 +41,7 @@ export default function HomePage({ data, params }) {
         <OurLandscapeSection />
         <PickSection className='pickSection2' />
         <BlogsSection />
-        <QuestionsSection />
+        <QuestionsSection /> */}
       </StyledHomeWrapper>
     </PageProvider>
   )
