f02bade823fda2a5398eb3dc455bd3c96a2da021
yaroslav <pashinskij99@gmail.com>
2023-10-06 05:38:08 +0300

    fix

--- src/views/HomePage/components/HomePageWrapper.jsx
+++ src/views/HomePage/components/HomePageWrapper.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useWindowSize } from '@uidotdev/usehooks'
 import CryptoWidgetSection from '@/sections/HomeSections/CryptoWidgetSection'
 import SmoothAPISection from '@/sections/HomeSections/SmoothAPISection'
 import PickSection from '@/sections/HomeSections/PickSection'
@@ -8,18 +9,30 @@ import FeesBusiness from '@/sections/HomeSections/FeesBusiness'
 import OurLandscapeSection from '@/sections/HomeSections/OurLandscapeSection'
 import BlogsSection from '@/sections/HomeSections/BlogsSection'
 import QuestionsSection from '@/sections/HomeSections/QuestionsSection'
+import { responseBreakPoint } from '@/utils/response'
 
 function HomePageWrapper() {
+  const size = useWindowSize()
   return (
     <>
+      {size.width && size.width <= responseBreakPoint.mobile ? (
+        <FeesBusiness modelId='2540165' autoId={10} />
+      ) : null}
       <CryptoWidgetSection /> {/* good */}
       <SmoothAPISection /> {/* good */}
       <PickSection className='pickSection' variant='dontLose' />
       <CryptoWidget2Section />
       <ReasonsToTeamUp />
-      <FeesBusiness modelId='2540165' autoId={10} />
+      {size.width && size.width <= responseBreakPoint.mobile ? (
+        <PickSection className='pickSection2' />
+      ) : null}
+      {size.width && size.width > responseBreakPoint.mobile ? (
+        <FeesBusiness modelId='2540165' autoId={10} />
+      ) : null}
       <OurLandscapeSection />
-      <PickSection className='pickSection2' />
+      {size.width && size.width > responseBreakPoint.mobile ? (
+        <PickSection className='pickSection2' />
+      ) : null}
       <BlogsSection />
       <QuestionsSection />
     </>
