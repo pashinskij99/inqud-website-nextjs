bc99fe8fda1e067cc2276a944c8f44d7137f3ff1
yaroslav <pashinskij99@gmail.com>
2023-10-06 18:42:24 +0300

    fx

--- src/views/CryptoWidget/CryptoWidget.jsx
+++ src/views/CryptoWidget/CryptoWidget.jsx
@@ -1,47 +1,24 @@
 'use client'
 
 import { StyledCryptoWidgetWrapper } from './CryptoWidget.styled'
-import CryptoWidgetHeroSection from '@/sections/CryptoWidgetSections/CryptoWidgetHeroSection'
-import CryptoWidgetFlexibility from '@/sections/CryptoWidgetSections/CryptoWidgetFlexibility'
-import CryptoBusinessSection from '@/sections/CryptoWidgetSections/CryptoBusinessSection'
-import CryptoWidgetFeatures from '@/sections/CryptoWidgetSections/CryptoWidgetFeatures'
-import FeesBusiness from '@/sections/HomeSections/FeesBusiness'
-import HowIntegrateCryptoSection from '@/sections/CryptoWidgetSections/HowIntegrateCryptoSection'
-import SafetyReliability from '@/sections/CryptoWidgetSections/SafetyReliability'
-import OurLandscapeSection from '@/sections/HomeSections/OurLandscapeSection'
-import QuestionsSection from '@/sections/HomeSections/QuestionsSection'
-import SendRequestSection from '@/sections/CryptoWidgetSections/SendRequestSection'
-import PageProvider from '@/contexts/PageContext/PageContext'
-import ConnectCryptoSection from '@/sections/CryptoWidgetSections/ConnectCryptoSection'
+// import CryptoWidgetHeroSection from '@/sections/CryptoWidgetSections/CryptoWidgetHeroSection'
+// import CryptoWidgetFlexibility from '@/sections/CryptoWidgetSections/CryptoWidgetFlexibility'
+// import CryptoBusinessSection from '@/sections/CryptoWidgetSections/CryptoBusinessSection'
+// import CryptoWidgetFeatures from '@/sections/CryptoWidgetSections/CryptoWidgetFeatures'
+// import FeesBusiness from '@/sections/HomeSections/FeesBusiness'
+// import HowIntegrateCryptoSection from '@/sections/CryptoWidgetSections/HowIntegrateCryptoSection'
+// import SafetyReliability from '@/sections/CryptoWidgetSections/SafetyReliability'
+// import OurLandscapeSection from '@/sections/HomeSections/OurLandscapeSection'
+// import QuestionsSection from '@/sections/HomeSections/QuestionsSection'
+// import SendRequestSection from '@/sections/CryptoWidgetSections/SendRequestSection'
 // import ConnectCryptoSection from '@/sections/CryptoWidgetSections/ConnectCryptoSection'
-
-// const DynamicCryptoWidgetHeroSection = dynamic(
-//   import('@/sections/CryptoWidgetSections/CryptoWidgetHeroSection').then(
-//     (res) => res.default
-//   ),
-//   {
-//     ssr: false,
-//   }
-// )
+import PageProvider from '@/contexts/PageContext/PageContext'
 
 export default function CryptoWidget({ data }) {
   return (
     <PageProvider dataPage={data} isFaq nameCMSPage='cryptoWidgetPage'>
       <StyledCryptoWidgetWrapper>
-        <CryptoWidgetHeroSection />
-        {/* <DynamicCryptoWidgetHeroSection /> */}
-        {/* <DynamicCryptoWidgetFlexibility />
-        <DynamicCryptoBusinessSection />
-        <DynamicCryptoWidgetFeatures />
-        <DynamicFeesBusiness modelId='2540254' />
-        <DynamicHowIntegrateCryptoSection />
-        <DynamicSafetyReliability />
-        <DynamicOurLandscapeSection />
-        <DynamicConnectCryptoSection />
-        <DynamicQuestionsSection />
-        <DynamicSendRequestSection /> */}
-
-        {/* <CryptoWidgetHeroSection /> */}
+        {/* <CryptoWidgetHeroSection />
         <CryptoWidgetFlexibility />
         <CryptoBusinessSection />
         <CryptoWidgetFeatures />
@@ -51,7 +28,7 @@ export default function CryptoWidget({ data }) {
         <OurLandscapeSection />
         <ConnectCryptoSection />
         <QuestionsSection />
-        <SendRequestSection />
+        <SendRequestSection /> */}
       </StyledCryptoWidgetWrapper>
     </PageProvider>
   )
