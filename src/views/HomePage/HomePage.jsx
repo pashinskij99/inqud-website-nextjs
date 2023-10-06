66b9d844520cce3456d021e4abfde88f930950ab
yaroslav <pashinskij99@gmail.com>
2023-10-06 16:20:23 +0300

    fx

--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -5,15 +5,15 @@ import { StyledHomeWrapper } from './HomePage.styled'
 import { NotFoundContext } from '@/contexts/NotFoundContext/NotFoundContext'
 import PageProvider from '@/contexts/PageContext/PageContext'
 import FeesBusiness from '@/sections/HomeSections/FeesBusiness'
-import CryptoWidgetSection from '@/sections/HomeSections/CryptoWidgetSection'
-import SmoothAPISection from '@/sections/HomeSections/SmoothAPISection'
+// import CryptoWidgetSection from '@/sections/HomeSections/CryptoWidgetSection'
+// import SmoothAPISection from '@/sections/HomeSections/SmoothAPISection'
 import PickSection from '@/sections/HomeSections/PickSection'
-import CryptoWidget2Section from '@/sections/HomeSections/CryptoWidget2Section'
+// import CryptoWidget2Section from '@/sections/HomeSections/CryptoWidget2Section'
 import ReasonsToTeamUp from '@/sections/HomeSections/ReasonsToTeamUp'
 import OurLandscapeSection from '@/sections/HomeSections/OurLandscapeSection'
 import BlogsSection from '@/sections/HomeSections/BlogsSection'
 import QuestionsSection from '@/sections/HomeSections/QuestionsSection'
-import HeroSection from '@/sections/HomeSections/HeroSection'
+// import HeroSection from '@/sections/HomeSections/HeroSection'
 import YourNeedsSection from '@/sections/HomeSections/YourNeedsSection'
 
 export default function HomePage({ data, params }) {
@@ -31,12 +31,12 @@ export default function HomePage({ data, params }) {
     <PageProvider isFaq dataPage={data} params={params} nameCMSPage='homePage'>
       <StyledHomeWrapper>
         {/* Content loading ssr */}
-        <HeroSection />
+        {/* <HeroSection /> */}
         <YourNeedsSection />
-        <CryptoWidgetSection /> {/* good */}
-        <SmoothAPISection /> {/* good */}
+        {/* <CryptoWidgetSection /> good */}
+        {/* <SmoothAPISection /> good */}
         <PickSection className='pickSection' variant='dontLose' />
-        <CryptoWidget2Section />
+        {/* <CryptoWidget2Section /> */}
         <ReasonsToTeamUp />
         <FeesBusiness modelId='2540165' autoId={10} />
         <OurLandscapeSection />
