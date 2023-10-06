925842816a82ad91acf92faa6c6cd853991964c5
yaroslav <pashinskij99@gmail.com>
2023-10-06 21:31:02 +0300

    fx

--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -4,17 +4,17 @@
 import { StyledHomeWrapper } from './HomePage.styled'
 // import { NotFoundContext } from '@/contexts/NotFoundContext/NotFoundContext'
 import PageProvider from '@/contexts/PageContext/PageContext'
-// import FeesBusiness from '@/sections/HomeSections/FeesBusiness'
-// import CryptoWidgetSection from '@/sections/HomeSections/CryptoWidgetSection'
-// import SmoothAPISection from '@/sections/HomeSections/SmoothAPISection'
-// import PickSection from '@/sections/HomeSections/PickSection'
-// import CryptoWidget2Section from '@/sections/HomeSections/CryptoWidget2Section'
-// import ReasonsToTeamUp from '@/sections/HomeSections/ReasonsToTeamUp'
-// import OurLandscapeSection from '@/sections/HomeSections/OurLandscapeSection'
-// import BlogsSection from '@/sections/HomeSections/BlogsSection'
-// import QuestionsSection from '@/sections/HomeSections/QuestionsSection'
+import FeesBusiness from '@/sections/HomeSections/FeesBusiness'
+import CryptoWidgetSection from '@/sections/HomeSections/CryptoWidgetSection'
+import SmoothAPISection from '@/sections/HomeSections/SmoothAPISection'
+import PickSection from '@/sections/HomeSections/PickSection'
+import CryptoWidget2Section from '@/sections/HomeSections/CryptoWidget2Section'
+import ReasonsToTeamUp from '@/sections/HomeSections/ReasonsToTeamUp'
+import OurLandscapeSection from '@/sections/HomeSections/OurLandscapeSection'
+import BlogsSection from '@/sections/HomeSections/BlogsSection'
+import QuestionsSection from '@/sections/HomeSections/QuestionsSection'
 import HeroSection from '@/sections/HomeSections/HeroSection'
-// import YourNeedsSection from '@/sections/HomeSections/YourNeedsSection'
+import YourNeedsSection from '@/sections/HomeSections/YourNeedsSection'
 
 export default function HomePage({ data, params }) {
   // const { setIsNotFound } = useContext(NotFoundContext)
@@ -31,17 +31,17 @@ export default function HomePage({ data, params }) {
     <PageProvider isFaq dataPage={data} params={params} nameCMSPage='homePage'>
       <StyledHomeWrapper>
         <HeroSection />
-        {/* <YourNeedsSection /> */}
-        {/* <CryptoWidgetSection /> */}
-        {/* <SmoothAPISection /> */}
-        {/* <PickSection className='pickSection' variant='dontLose' /> */}
-        {/* <CryptoWidget2Section /> */}
-        {/* <ReasonsToTeamUp /> */}
-        {/* <FeesBusiness modelId='2540165' autoId={10} /> */}
-        {/* <OurLandscapeSection /> */}
-        {/* <PickSection className='pickSection2' /> */}
-        {/* <BlogsSection /> */}
-        {/* <QuestionsSection /> */}
+        <YourNeedsSection />
+        <CryptoWidgetSection />
+        <SmoothAPISection />
+        <PickSection className='pickSection' variant='dontLose' />
+        <CryptoWidget2Section />
+        <ReasonsToTeamUp />
+        <FeesBusiness modelId='2540165' autoId={10} />
+        <OurLandscapeSection />
+        <PickSection className='pickSection2' />
+        <BlogsSection />
+        <QuestionsSection />
       </StyledHomeWrapper>
     </PageProvider>
   )
