2382d992bdb794fb51202dc02c5e5579ebe24b6d
yaroslav <pashinskij99@gmail.com>
2023-10-06 18:46:01 +0300

    fx

--- src/views/CompanyPage/CompanyPage.jsx
+++ src/views/CompanyPage/CompanyPage.jsx
@@ -1,25 +1,18 @@
 'use client'
 
-import CompanyHeroSection from '@/sections/CompanySections/CompanyHeroSection/CompanyHeroSection'
 import { StyledCompanyPageWrapper } from './CompanyPage.styled'
+import PageProvider from '@/contexts/PageContext/PageContext'
+// import CompanyHeroSection from '@/sections/CompanySections/CompanyHeroSection/CompanyHeroSection'
 // import CoreSection from '@/sections/CompanySections/CoreSection'
 // import LandscapeSection from '@/sections/CompanySections/LandscapeSection/LandscapeSection'
-// import TeamSection from '@/sections/CompanySections/TeamSection'
 // import FeaturesSection from '@/sections/CompanySections/FeaturesSection'
-import PageProvider from '@/contexts/PageContext/PageContext'
-import CoreSection from '@/sections/CompanySections/CoreSection'
-import LandscapeSection from '@/sections/CompanySections/LandscapeSection/LandscapeSection'
-import FeaturesSection from '@/sections/CompanySections/FeaturesSection'
 
 export default function CompanyPage({ data }) {
   return (
     <PageProvider dataPage={data} nameCMSPage='aboutUsPage'>
       <StyledCompanyPageWrapper>
-        <CompanyHeroSection />
+        {/* <CompanyHeroSection />
         <CoreSection />
-        <LandscapeSection />
-        <FeaturesSection />
-        {/* <CoreSection />
         <LandscapeSection />
         <FeaturesSection /> */}
       </StyledCompanyPageWrapper>
