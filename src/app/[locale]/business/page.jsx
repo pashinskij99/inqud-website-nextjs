bb489f986afbd72879e083e6dc0d8e80d3835109
yaroslav <pashinskij99@gmail.com>
2023-10-09 14:13:16 +0300

    fx

--- src/app/[locale]/business/page.jsx
+++ src/app/[locale]/business/page.jsx
@@ -1,32 +1,5 @@
-/* eslint-disable no-unused-vars */
 import HomePage from '@/views/HomePage'
-import { getData } from '@/lib/datocms'
-
-const HOME_PAGE_QUERY = `
-  query MyQuery($locale: SiteLocale) {
-    homePage(locale: $locale) {
-      buttonScreen1ContactSales
-      buttonScreen1GetStarted
-      description {
-        value
-      }
-      features
-      id
-      subTitle
-      title
-      buttonScreen2
-    }
-  }
-`
 
 export default async function Home({ params }) {
-  // const data = await getData(HOME_PAGE_QUERY, {
-  //   locale: params.locale,
-  // })
-  return (
-    <HomePage
-      // data={data}
-      params={params}
-    />
-  )
+  return <HomePage params={params} />
 }
