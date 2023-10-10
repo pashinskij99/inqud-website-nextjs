7da8d7537b973b211c3c4d23b2be4b5731e3dbc8
yaroslav <pashinskij99@gmail.com>
2023-10-10 23:28:00 +0300

    fx

--- src/sections/HomeSections/HeroSection/components/AnimatedMobile.jsx
+++ src/sections/HomeSections/HeroSection/components/AnimatedMobile.jsx
@@ -1,24 +1,18 @@
-'use client'
-
 import clsx from 'clsx'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
 import styles from '../HeroSection.module.scss'
 
 export default function AnimatedMobile() {
-  const [stop, setStop] = useState(false)
-  useEffect(() => {
-    setTimeout(() => {
-      setStop(true)
-    }, 2000)
-  }, [])
-
   return (
     <div className={styles['gif-wrapper']}>
       <Image
-        className={clsx(styles.gif, styles.gif_1, {
-          [styles.hide]: stop,
-        })}
+        className={clsx(
+          styles.gif,
+          styles.gif_1
+          //   , {
+          //   [styles.hide]: stop,
+          // }
+        )}
         src='/video/b2b_hero_mobile.avif'
         alt='avif'
         priority
@@ -28,9 +22,13 @@ export default function AnimatedMobile() {
         height={152}
       />
       <Image
-        className={clsx(styles.gif, styles.gif_2, {
-          [styles.hide]: !stop,
-        })}
+        className={clsx(
+          styles.gif,
+          styles.gif_2
+          //   , {
+          //   [styles.hide]: !stop,
+          // }
+        )}
         priority
         fetchpriority='high'
         loading='eager'
