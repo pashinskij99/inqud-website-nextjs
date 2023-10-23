000f1dbf0f3c4f68de1758cec4d98e3ce1ba0be6
yaroslav <pashinskij99@gmail.com>
2023-10-23 12:51:19 +0300

    change language

--- src/utils/font.js
+++ src/utils/font.js
@@ -1,6 +1,14 @@
-import { Nunito } from 'next/font/google'
+// eslint-disable-next-line camelcase
+import { Noto_Sans } from 'next/font/google'
 
-export const nunito = Nunito({
-  subsets: ['cyrillic', 'latin'],
+export const nunito = Noto_Sans({
+  // subsets: ['cyrillic', 'latin'],
+  subsets: ['latin'],
+  // weight: ['400', '500', '700'],
   weight: ['400', '500', '700'],
 })
+
+// export const notoSans = Noto_Sans({
+//   subsets: ['cyrillic', 'latin'],
+//   weight: ['400', '500', '700'],
+// })
