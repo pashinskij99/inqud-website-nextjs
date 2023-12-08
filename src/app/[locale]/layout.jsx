325261ec4a4765b9dd385a172fcc711487220181
pashinskij99 <pashinskij99@gmail.com>
2023-12-08 10:33:11 +0200

    add chat Intercom

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -59,6 +59,20 @@ export default async function RootLayout({ children, params: { locale } }) {
            })(window,document,'script','dataLayer','GTM-5J4TMBX'); 
          `}
         </Script>
+        {/*  add Chat */}
+        <Script id='chat-1'>
+          {`window.intercomSettings = {
+            api_base: "https://api-iam.intercom.io",
+            app_id: "e5plnjcu"
+          };`}
+        </Script>
+
+        <Script id='chat-2'>
+          {`
+            (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/e5plnjcu';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+          `}
+        </Script>
+        {/*  add Chat */}
         <noscript>
           <iframe
             title='googletagmanager'
