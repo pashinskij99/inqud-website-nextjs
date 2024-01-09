b13f1c844b9805cbec3098189a908c1ab32d8d55
pashinskij99 <pashinskij99@gmail.com>
2024-01-09 17:32:34 +0200

    add new route logic

--- src/app/api/create-contact/route.js
+++ src/app/api/create-contact/route.js
@@ -6,6 +6,51 @@ const apiKey = process.env.NEXT_PUBLIC_ACTIVECAMPAIGN_API_KEY
 export async function POST(req) {
   const { contact, automationId } = await req.json()
 
+  const filterResponse = await fetch(
+    `${apiUrl}/api/3/contacts?email=${contact.email}`,
+    {
+      headers: {
+        'Api-Token': apiKey,
+      },
+    }
+  )
+
+  const filterData = await filterResponse.json()
+
+  if (filterData.contacts.length) {
+    const response = await fetch(
+      `${apiUrl}/api/3/contacts/${filterData.contacts[0].id}`,
+      {
+        method: 'PUT',
+        headers: {
+          'Api-Token': apiKey,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ contact }),
+      }
+    )
+
+    const dataRes = await response.json()
+    const contactAutomation = {
+      contact: dataRes.contact.id,
+      automation: automationId,
+    }
+
+    const responseAutomation = await fetch(
+      `${apiUrl}/api/3/contactAutomations`,
+      {
+        method: 'POST',
+        headers: {
+          'Api-Token': apiKey,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ contactAutomation }),
+      }
+    )
+
+    return NextResponse.json({ dataRes, responseAutomation })
+  }
+
   const response = await fetch(`${apiUrl}/api/3/contacts`, {
     method: 'POST',
     headers: {
@@ -14,7 +59,6 @@ export async function POST(req) {
     },
     body: JSON.stringify({ contact }),
   })
-
   const dataRes = await response.json()
   const contactAutomation = {
     contact: dataRes.contact.id,
